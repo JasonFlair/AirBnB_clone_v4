61027cb6e5823aa99c2de00f4f327292606a7490
JasonFlair <emelieobumse100@gmail.com>
2023-01-27 20:13:36 +0100

    amenities

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -14,6 +14,6 @@ $(document).ready(function() {
 
   function updateAmenitiesHeader() {
     let amenitiesString = Object.values(checkedAmenities).join(', ');
-    $('h4#amenities_checked').text(amenitiesString);
+    $('#amenities_checked h4').text(amenitiesString);
   }
 });
