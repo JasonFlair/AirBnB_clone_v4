30f99001b606f75305a3983fac32e0402b36d653
JasonFlair <emelieobumse100@gmail.com>
2023-01-27 20:09:06 +0100

    amenities

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  let checkedAmenities = {};
+
+  $('input:checkbox').change(function() {
+    let amenityId = $(this).data('id');
+    let amenityName = $(this).data('name');
+    if ($(this).is(':checked')) {
+      checkedAmenities[amenityId] = amenityName;
+    } else {
+      delete checkedAmenities[amenityId];
+    }
+    updateAmenitiesHeader();
+  });
+
+  function updateAmenitiesHeader() {
+    let amenitiesString = Object.values(checkedAmenities).join(', ');
+    $('#amenities h4').text(amenitiesString);
+  }
+});
