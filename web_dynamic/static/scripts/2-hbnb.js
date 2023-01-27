9da56a809e0b45769e4f25a00d2fcef5d09c9927
JasonFlair <emelieobumse100@gmail.com>
2023-01-27 23:01:19 +0100

    changes

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  $.get('http://0.0.0.0:5001/api/v1/status/', function(status) {
+  if (data.status == 'OK') {
+    $('div#api_status').addClass('available');
+  } else {
+    $('div#api_status').removeClass('available');
+  }
+  });
+
+
+});
+
+$(document).ready(function() {
+  let checkedAmenities = {};
+
+  $('input:checkbox').change(function() {
+    let amenityId = $(this).data('id');
+    let amenityName = $(this).data('name');
+    if ($(this).is(':checked')) {
+      checkedAmenities[amenityId] = amenityName;
+    } else {
+      delete checkedAmenities[amenityId];
+    }
+    let amenitiesString = Object.values(checkedAmenities).join(', ');
+    $('.amenities h4').text(amenitiesString);
+  });
+});
