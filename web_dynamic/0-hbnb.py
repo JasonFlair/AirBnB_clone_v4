46d08a3114d83b808d71c29995d9d3ad6cf2d834
JasonFlair <emelieobumse100@gmail.com>
2023-01-27 17:35:57 +0100

    query

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -21,6 +21,9 @@ def close_db(error):
 @app.route('/0-hbnb', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
+    from uuid import uuid4
+
+    cache_id = uuid4()
     states = storage.all(State).values()
     states = sorted(states, key=lambda k: k.name)
     st_ct = []
@@ -37,7 +40,8 @@ def hbnb():
     return render_template('0-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places)
+                           places=places,
+                           cache_id=cache_id)
 
 
 if __name__ == "__main__":
