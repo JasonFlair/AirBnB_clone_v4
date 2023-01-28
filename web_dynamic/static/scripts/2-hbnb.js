a1573fb1e5868dfe8467c83b2eae13ebc5933b4d
JasonFlair <emelieobumse100@gmail.com>
2023-01-28 09:36:07 +0100

    mob

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   const url = 'http://' + location.hostname + ':5001/api/v1/status/';
+  console.log(url)
   $.get(url, function(data) {
   if (data.status === 'OK') {
     $('div#api_status').addClass('available');
